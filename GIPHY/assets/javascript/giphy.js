70b8b3fc62522e0e051c72e2c6139247721680bc
Trie2168 <trie0037@gmail.com>
2018-06-18 12:40:08 -0500

    Giphy2

--- GIPHY/assets/javascript/giphy.js
+++ GIPHY/assets/javascript/giphy.js
@@ -1,45 +1,5 @@
-/*function showTopics(data) {
-    var topics = '';
-}
-
-showTopics({
-    "topics": [
-        {"Islands": "Caribbean"},
-        {"Islands": "Mediterranean"},
-        {"Islands": "Eleutian"}
-    ]
-   });
-*/
-
-/*var topics = [{
-    "Superheroes": "",//would like to be able to add any superhero if possible
-}]
-
-function topicButtons() {
-
-    // Deletes the superheroes prior to adding new superheroes
-    // (this is necessary otherwise you will have repeat buttons)
-    $("#buttons-view").empty();
-
-    // Loops through the array of superheroes
-    for (var i = 0; i < topics.length; i++) {
-
-        // Then dynamicaly generates buttons for each hero in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-        var b = $("<button>");
-        // Adds a class of movie to our button
-        b.addClass("superhero");
-        // Added a data-attribute
-        b.attr("data-name", movies[i]);
-        // Provided the initial button text
-        b.text(movies[i]);
-        // Added the button to the buttons-view div
-        $("#buttons-view").append(b);
-    }
-}*/
-
 // Initial array of movies that superheroes are in:
-var movies = ["Batman"];
+var movies = ["Superheroes"];
 
 // displayMovieInfo function re-renders the HTML to display the appropriate content
 function displayMovieInfo() {
@@ -122,7 +82,7 @@ $(document).on("click", ".movie", displayMovieInfo);
 
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
-$(document).on("click", ".gif", function() {
+$(document).on("click", ".gif", function () {
     // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
     var state = $(this).attr("data-state");
     // If the clicked image's state is still, update its src attribute to what its data-animate value is.
@@ -130,41 +90,11 @@ $(document).on("click", ".gif", function() {
     // Else set src to the data-still value
     console.log(this)
     if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
     } else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
     }
-  });
-
-/*$("button").on("click", function () {
-    var person = $(this).attr("data-person");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        person + "&api_key=XOENw94Vovq4Mjfk4D0WUe1j6b8qP1Cn&limit=10";
-
-    $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        .then(function (response) {
-            var results = response.data;
-
-            for (var i = 0; i < results.length; i++) {
-                var gifDiv = $("<div class='item'>");
-
-                var rating = results[i].rating;
-
-                var p = $("<p>").text("Rating: " + rating);
-
-                var personImage = $("<img>");
-                personImage.attr("src", results[i].images.fixed_height.url);
-                
-                gifDiv.prepend(p);
-                gifDiv.prepend(personImage);
-
-                $("#gifs-appear-here").prepend(gifDiv);
-            }
-        });
 });
-*/
+
